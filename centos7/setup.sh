f529828f35aac14850e4fb18baf73d4377d1133a
sthdev <sthdev@posteo.de>
2020-02-16 11:57:59 +0100

    initial ccentos7 config

---
+++ centos7/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+sudo localectl set-keymap de-latin1
+
+# Install EPEL repositories
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# Update installed packages
+sudo yum -y update
+
+# Install Git
+
+# Install Python
